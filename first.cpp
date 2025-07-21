64231302fb1c4b7a3235d7d0e13a4c5495f201a1
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-07-21 15:41:34 -0600

    first cpp program

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int cur_year = 2025;
+    std::cout << "Enter your birth year: " << std::endl;
+    int birth_year = 0;
+    std::cin >> birth_year;
+    std::cout << "You are " << cur_year - birth_year << " years old" << std::endl;
+    return 0;
+}
+// first program :skull: absolute dogwater?
+
+
+
