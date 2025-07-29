54970f77e89b52ae12936c1a9553e47a9c4010fc
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-07-29 16:09:28 -0600

    added documentation for Sales_item.h

--- chapter 1/bookstore/Sales_item.h
+++ chapter 1/bookstore/Sales_item.h
@@ -10,14 +10,19 @@ public:
     float price_sold;
     float total_revenue;
 
+    // override for cin of Sales_item
     friend std::istream& operator>> (std::istream& is, Sales_item& item);
 
+    // override for cout of Sales_item
     friend std::ostream& operator<< (std::ostream& os, Sales_item& item);
 
     // only valid if IBN of first and second match
     friend Sales_item& operator+ (Sales_item& first, Sales_item& second);
+
+    // returns average
     float get_average();
 
+    // prints information, depreciated as expected usage utilizes << operator
     void print_info();
 };
 
