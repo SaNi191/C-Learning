c21084c87fe4d2bc9fbca117f941963149a9f629
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-07-29 16:18:42 -0600

    completed exercise for 3.1

--- chapter 3/exercises.cpp
+++ chapter 3/exercises.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
-
+#include "sales_data.h"
+// add tag -I ../chapter\ 2 to compilation
+// do not include ../chapter\ 2/sales_data.cpp
 using std::cin; using std::cout; using std::endl;
 
 int main () {
+
+    // 1.4.1 with using declarations
     int lbound, hbound;
     cout << "Lower Bound: ";
     cin >> lbound;
@@ -16,5 +20,13 @@ int main () {
     }
 
     cout << "Sum: " << sum << endl;
-    
+
+    // 2.6.2 with using declarations
+    Sales_data my_data;
+    cin >> my_data.IBN >> my_data.price >> my_data.revenue >> my_data.sales;
+    cout << "IBN: " << my_data.IBN << endl;
+    cout << "Price: $" << my_data.price << endl;
+    cout << "Revenue: $" << my_data.revenue << endl;
+    cout << "Total Sales: " << my_data.sales << endl;
+
 }
