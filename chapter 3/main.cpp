ffe2e745d0463718db971926db95437f09e1a188
SaNi191 <143741958+SaNi191@users.noreply.github.com>
2025-07-31 09:48:04 -0600

    completed getline exercise, string.size stored in string::size_t exercise

--- chapter 3/main.cpp
+++ chapter 3/main.cpp
@@ -1,1 +1,28 @@
 #include <iostream>
+#include <string>
+
+
+using std::string; using std::cin; using std::cout; using std::endl; using std::getline;
+
+int main() {
+    // read until end-of-file
+    cout << "1) Line\n2) Word" << endl;
+    int choice;
+    cin >> choice;
+   
+    
+    if (choice == 1) {
+        string line;
+        while (getline(cin, line)) {
+            auto line_size = line.size();
+            cout << line << " (" << line_size  << ")" << endl;
+        }
+    } else {
+        string word;
+        while (cin >> word) {
+            cout << word << endl; // write each word followed by a new line
+        }
+    }
+   
+    return 0;
+}
